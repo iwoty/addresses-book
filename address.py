b5e625c69341ca49d74dc07a4dc0d89f3b3d5dca
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:24:08 +0200

    Add __eq__ method that check if two Address instances are equal.

--- address.py
+++ address.py
@@ -9,3 +9,7 @@ class Address:
 
     def get_full_address(self):
         return '{}, {}, {} {}'.format(self.person, self.city, self.street, str(self.house_no))
+
+    def __eq__(self, other):
+        return ((self.person, self.city, self.street, self.house_no)
+                == (other.person, other.city, other.street, other.house_no))
