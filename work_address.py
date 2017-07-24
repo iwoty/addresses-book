dd2aa33faf4da10b4aa664c1705ee2cb2311893d
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:25:52 +0200

    Add get_full_address method that returns string with full address.

--- work_address.py
+++ work_address.py
@@ -7,3 +7,6 @@ class WorkAddress(Address):
     def __init__(self, person, city, street, house_no, company):
         super().__init__(person, city, street, house_no)
         self.company = company
+
+    def get_full_address(self):
+        return '{}, {}, {} {}, {}'.format(self.person, self.city, self.street, str(self.house_no), self.company)
