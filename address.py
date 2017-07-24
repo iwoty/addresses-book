b15c8c3a125b9dc20ba0b948db881bdcf4690505
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:22:40 +0200

    Add class Address with its init method

---
+++ address.py
@@ -0,0 +1,8 @@
+
+class Address:
+
+    def __init__(self, person, city, street, house_no):
+        self.person = person
+        self.city = city
+        self.street = street
+        self.house_no = house_no
