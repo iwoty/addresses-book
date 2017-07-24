cbe52b191b94dcf4ac7ae8e771df6f0e3388a800
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:30:27 +0200

    Add add_address method that adds addres to addresses list.

--- address_book.py
+++ address_book.py
@@ -4,3 +4,11 @@ class AddressBook:
     def __init__(self, name):
         self.name = name
         self.addresses = []
+
+    def add_address(self, address):
+        if address.__class__.__name__ == 'Address':
+            self.addresses.append(address)
+        elif address.__class__.__name__ == 'WorkAddress':
+            self.addresses.append(address)
+        else:
+            raise TypeError('Test dupy się nie powiódł ;(')
