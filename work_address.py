bf3f0d4cd825e4f9ddea2b9eac6496ae3dbf9c9c
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:26:33 +0200

    Add __eq__ method that check if two Address or WordAddress instances are equal.

--- work_address.py
+++ work_address.py
@@ -10,3 +10,11 @@ class WorkAddress(Address):
 
     def get_full_address(self):
         return '{}, {}, {} {}, {}'.format(self.person, self.city, self.street, str(self.house_no), self.company)
+
+    def __eq__(self, other):
+        if self.__class__.__name__ != other.__class__.__name__:
+            return ((self.person, self.city, self.street, self.house_no)
+                    == (other.person, other.city, other.street, other.house_no))
+        else:
+            return ((self.person, self.city, self.street, self.house_no, self.company)
+                    == (other.person, other.city, other.street, other.house_no, other.company))
