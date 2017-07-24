1f37236ada258e013b97a3deef0cb21b5b424d68
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:43:08 +0200

    Add get_anykey method which is waiting for enter to be pressed (keeps the program in place.

--- user_input.py
+++ user_input.py
@@ -10,3 +10,9 @@ class UserInput:
         user_input_number = input("\nPick an option (number): ")
         while not user_input_number.isnumeric():
             user_input_number = input("\nPick an PROPER option (number): ")
+
+    def get_anykey(self):
+        """
+        Returns input that waits for anykey.
+        """
+        return input('Press -->ENTER<-- to move forward.')
