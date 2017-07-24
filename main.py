8dbeb15043596b607239082d38d6ea01e312af25
Iwo <iwo.tyszkowski@gmail.com>
2017-07-24 11:32:04 +0200

    Add main file with entry point to application.

---
+++ main.py
@@ -0,0 +1,13 @@
+from application import Application
+
+
+def main():
+    """
+    Entry point for the application.
+    """
+    app = Application()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
